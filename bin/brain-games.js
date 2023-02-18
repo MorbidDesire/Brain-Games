477d9101a231a8caa5f649f99dd29860fa520fe7
Alexander <Ra1jinThunderkeg@yandex.ru>
2023-02-18 15:04:15 +0300

    linting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import pogname from "../src/cli.js"
-console.log("Welcome to the Brain Games!");
-pogname ();
+import pogname from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+pogname();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const pogname = () => {
-    var userName = readlineSync.question("May I have your name? ");
-    console.log("Hello, " + userName + "!");
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default pogname;
