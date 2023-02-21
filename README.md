1d76ed56d917fa0f7e20625915d03b59be0b46bc
Alexander <Ra1jinThunderkeg@yandex.ru>
2023-02-21 13:08:54 +0300

    add brain-calc

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Actions Status](https://github.com/MorbidDesire/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/MorbidDesire/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/99ed9c3dc51e2d996625/maintainability)](https://codeclimate.com/github/MorbidDesire/frontend-project-44/maintainability)
 https://asciinema.org/a/pkKDQua5FsbZ0Em6hXBwNvmXH
+https://asciinema.org/a/nOYbGDqbjx2mNbhqaVfwnDQVL
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,33 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import evengame from '../src/games/brain-even.js';
 
-const firstNumber = Math.floor(Math.random() * 100);
-const secondNumber = Math.floor(Math.random() * 100);
-const thirdNumber = Math.floor(Math.random() * 100);
-const tries = [firstNumber, secondNumber, thirdNumber];
-
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-console.log("Answer 'yes' if the number is even, otherwise answer 'no'");
-for (let i = 0; i < tries.length; i += 1) {
-  console.log(`Question: ${tries[i]}`);
-  const answer = readlineSync.question('Your answer: ');
-  // eslint-disable-next-line no-cond-assign
-  const evenNumber = tries[i] % 2 === 0;
-  const oddNumber = tries[i] % 2 !== 0;
-  if ((answer === 'yes' && evenNumber) || (answer === 'no' && oddNumber)) {
-    console.log('Correct!');
-  } else if (evenNumber) {
-    console.log(`'${answer}' is wrong answer. Correct answer was 'yes'`);
-    console.log(`Let's try again, ${userName}!`);
-    break;
-  } else {
-    console.log(`'${answer}' is wrong answer. Correct answer was 'no'`);
-    console.log(`Let's try again, ${userName}`);
-    break;
-  }
-  if (i === tries.length - 1) {
-    console.log(`Congratulations, ${userName}`);
-  }
-}
+evengame();
